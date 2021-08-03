c636f2c66c602e17022fd92db722c71f6f531340
jev001 <taojing901@gmail.com>
2021-08-03 13:34:08 +0800

    feat: wsl 编译辅助

--- build-jonah.sh
+++ build-jonah.sh
@@ -4,9 +4,10 @@
 export JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF8
 
 build_dir=`readlink -f ~/local/jdk/build`
-echo "链接本地目录. 加速编译 http://openjdk.java.net/groups/build/doc/building.html#Disk%20Speed"
-echo "ln -s $build_dir `readlink -f $(dirname $0)`/build"
-ln -s $build_dir `readlink -f $(dirname $0)`/build
+if [! -d $build_dir ];then
+  echo "链接本地目录. 加速编译 http://openjdk.java.net/groups/build/doc/building.html#Disk%20Speed"
+  echo "ln -s $build_dir `readlink -f $(dirname $0)`/build"
+  ln -s $build_dir `readlink -f $(dirname $0)`/build
 cmd="./build-with-wsl.sh --enable-debug --with-boot-jdk="`readlink -f ~/scripts/jdk-14.0.2/`
 echo "cmd="$cmd
 ./$cmd
